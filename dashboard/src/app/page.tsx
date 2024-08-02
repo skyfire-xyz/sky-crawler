5a71354aa82bc2c4422dafd4899ac3e708ee30b4
Chloe Ta <cqta@ucsb.edu>
2024-08-01 18:14:06 -0700

    added time

--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -4,24 +4,22 @@ import "@/src/globals.css";
 import Pusher from "pusher-js";
 import { useEffect, useState } from "react";
 import "./App.css";
-import Collapsible from "./components/collapsible";
 import SearchBar from "./components/SearchBar";
 import ShowTextButton from "./components/ShowTextButton";
 
-const CrawlingStatus = ({ currentSite }: { currentSite: MessageData | undefined }) => (
-  <div className="mb-4 rounded-lg border border-gray-300 bg-gray-50 p-4 dark:border-gray-600 dark:bg-gray-700">
-    <h2 className="mb-2 text-xl font-bold dark:text-white">Currently Crawling:</h2>
-    <p className="dark:text-white">{currentSite?.url}</p>
-  </div>
-);
 
 const LogList = ({ log }: { log: MessageData[] }) => (
-  <div className="grow rounded-lg border border-gray-300 bg-gray-50 p-4 dark:border-gray-600 dark:bg-gray-700">
-    <h2 className="mb-2 text-xl font-bold dark:text-white">Log:</h2>
+  <div className="flex-grow p-4 bg-gray-50 border border-gray-300 rounded-lg dark:bg-gray-700 dark:border-gray-600">
+    <h2 className="text-xl font-bold mb-2 dark:text-white">Crawled Data</h2>
     <ul>
       {log.map((entry, index) => (
-        <li key={index} className="mb-2 flex items-center justify-between">
-          <span className="dark:text-white">{entry.title}</span>
+        <li key={index} className="flex items-center justify-between mb-2">
+          <span className="text-custom-teal mr-2" style={{ color: '#0D7490' }}>
+            {entry.time}ms
+          </span>
+          <span className="flex-1 dark:text-white">
+            {entry.url}
+          </span>
           <ShowTextButton text={entry.text} />
         </li>
       ))}
@@ -29,15 +27,17 @@ const LogList = ({ log }: { log: MessageData[] }) => (
   </div>
 );
 
+
 const PaymentLog = ({ payments }: { payments: MessageData[] }) => (
-  <div className="w-1/3 rounded-lg border border-gray-300 bg-gray-50 p-4 dark:border-gray-600 dark:bg-gray-700">
-    <h2 className="mb-2 text-xl font-bold dark:text-white">Payment Protocol Logs</h2>
+  <div className="w-1/3 p-4 bg-gray-900 text-white border border-blue-800 rounded-lg dark:bg-white dark:text-gray-900 dark:border-gray-300">
+    <h2 className="text-xl font-bold mb-2">Payment Protocol Logs</h2>
     <ul>
       {payments.map((payment, index) => (
         <li key={index} className="dark:text-white">{payment.text}</li>
       ))}
     </ul>
   </div>
+
 );
 
 interface MessageData {
@@ -45,6 +45,7 @@ interface MessageData {
   title: string;
   url: string;
   text: string;
+  time: string;
 }
 
 export default function App() {
@@ -85,7 +86,6 @@ export default function App() {
         <SearchBar />
       </div>
       <div className="grow p-4">
-        <CrawlingStatus currentSite={currentSite} />
         <div className="flex space-x-4">
           <LogList log={log} />
           <PaymentLog payments={payments} />

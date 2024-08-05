a5d53fe5ba34cce30ebce68b67e1865a52e9f961
chloeqta <cqta@ucsb.edu>
2024-08-05 14:36:02 -0700

    output receipt in logs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,22 +38,25 @@ const LogList = ({ log }: { log: MessageData[] }) => (
   </div>
 );
 
-const PaymentLog = ({ payments }: { payments: MessageData[] }) => (
+const PaymentLog = ({ logs }: { logs: MessageData[] }) => (
   <div className="w-1/3 rounded-lg border border-blue-800 bg-gray-900 p-4 text-white dark:border-gray-300 dark:bg-white dark:text-gray-900">
     <h2 className="mb-2 text-xl font-bold">Payment Protocol Logs</h2>
     <ul>
-      {payments.map((payment, index) => (
+      {logs.map((log, index) => (
         <li key={index} className="mb-1.5 flex items-start justify-between">
           <div className="flex-1">
             <div className="flex flex-col">
               <span className="text-gray-200 dark:text-gray-400">
-                {payment.amount} USD paid to {payment.receiverUsername}
+                {log.senderUsername} paid {log.amount} USD to{" "}
+                {log.receiverUsername}
               </span>
-              <div className="flex items-center">
-                <span className="mr-3 text-xs text-[#0eb8c4]">
-                  Access granted to {payment.path}
-                </span>
-              </div>
+              {log.type === "receipt" ? null : (
+                <div className="flex items-center">
+                  <span className="mr-3 text-xs text-[#0eb8c4]">
+                    Access granted to {log.path}
+                  </span>
+                </div>
+              )}
             </div>
           </div>
         </li>
@@ -72,6 +75,7 @@ interface MessageData {
   time: string;
 
   amount: string;
+  senderUsername: string;
   receiverUsername: string;
   path: string;
 }
@@ -80,10 +84,12 @@ export default function App() {
   const [currentSite, setCurrentSite] = useState<MessageData>();
   const [log, setLog] = useState<MessageData[]>([]);
   const [payments, setPayments] = useState<MessageData[]>([]);
+  const [receipts, setReceipts] = useState<MessageData[]>([]);
 
   const handleSearch = () => {
     setLog([]);
     setPayments([]);
+    setReceipts([]);
   };
 
   useEffect(() => {
@@ -93,11 +99,17 @@ export default function App() {
     const channel = pusher.subscribe("my-channel");
     channel.bind("my-event", (data: { message: MessageData }) => {
       if (data.message !== undefined) {
-        if (data.message.type === "page") {
-          setCurrentSite(data.message);
-          setLog((prevLog) => [data.message, ...prevLog]);
-        } else {
-          setPayments((prevPayments) => [data.message, ...prevPayments]);
+        switch (data.message.type) {
+          case "page":
+            setCurrentSite(data.message);
+            setLog((prevLog) => [data.message, ...prevLog]);
+            break;
+          case "payment":
+            setPayments((prevPayments) => [data.message, ...prevPayments]);
+            break;
+          case "receipt":
+            setReceipts((prevReceipts) => [data.message, ...prevReceipts]);
+            break;
         }
       }
     });
@@ -105,6 +117,8 @@ export default function App() {
       pusher.unsubscribe("my-channel");
     };
   }, []);
+
+  const combinedLogs = [...payments, ...receipts];
 
   return (
     <div className="flex min-h-screen flex-col">
@@ -122,7 +136,7 @@ export default function App() {
       <div className="grow p-4">
         <div className="flex space-x-4">
           <LogList log={log} />
-          <PaymentLog payments={payments} />
+          <PaymentLog logs={combinedLogs} />
         </div>
       </div>
     </div>

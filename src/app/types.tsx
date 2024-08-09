b00f46a0a161380980f1912672163af636c2851c
Chloe Ta <cqta@ucsb.edu>
2024-08-08 18:43:34 -0700

    default values

--- src/app/types.tsx
+++ src/app/types.tsx
@@ -23,11 +23,15 @@ export enum AlertType {
   NETWORK = "network",
 }
 
-export enum AlertMessage {
-  MISSING_API = "Please enter an API key and try again.",
-  MISSING_URL = "Please input a valid URL and try again.",
-  INVALID_API = "Error processing request. Please check your API key and try again.",
-  BACKEND_DOWN = "Unable to reach the server. Please check your connection and try again",
-  DEFAULT_DEPTH = "No maximum depth specified. Defaulting to depth 3.",
-  DEFAULT_COST = "No maximum cost specified. Defaulting to 0.01 USD.",
-}
+export const AlertMessage = {
+  MISSING_API: "Please enter an API key and try again.",
+  MISSING_URL: "Please input a valid URL and try again.",
+  INVALID_API:
+    "Error processing request. Please check your API key and try again.",
+  BACKEND_DOWN:
+    "Unable to reach the server. Please check your connection and try again",
+  DEFAULT_DEPTH: `No maximum depth specified. Defaulting to depth ${DEFAULT_DEPTH}.`,
+  DEFAULT_COST: `No maximum cost specified. Defaulting to ${DEFAULT_COST} USD.`,
+} as const;
+
+export type AlertMessageType = keyof typeof AlertMessage;

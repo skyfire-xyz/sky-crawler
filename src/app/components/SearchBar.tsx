9bc49b5d54e35a67f4f03e8493f8ee1191223a4b
Chloe Ta <cqta@ucsb.edu>
2024-10-07 15:16:10 -0700

    fixed white circle in floating input fields

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -115,15 +115,15 @@ const SearchBar: React.FC<SearchBarProps> = ({
         <input
           type="text"
           id="floating_outlined"
-          className="border-1 peer block w-full appearance-none rounded-lg border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white"
-          placeholder=" " // Placeholder is kept for floating effect
+          className="border-1 peer block w-full appearance-none rounded-lg border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
+          placeholder=" "
           required
           value={inputUrl}
           onChange={handleInputChange}
         />
         <label
           htmlFor="floating_outlined"
-          className="absolute top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-white px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:px-2 peer-focus:text-blue-600 dark:bg-gray-900 peer-focus:dark:text-blue-500"
+          className="absolute start-1 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-white px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 dark:bg-gray-900 dark:text-gray-400 peer-focus:dark:text-blue-500 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
         >
           Website to crawl
         </label>
